2f9ef6aa5409a83d904b8ecb99673b28bc649ff9
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2019-03-26 13:29:27 +0100

    Change webpage layout (#1)
    * change layout of the webpage
    
    * add red output when error occurs
    
    * code refactor
    
    Co-authored-by: agnieszka-miszkurka <miszkur@student.agh.edu.pl>

--- static/main.js
+++ static/main.js
@@ -2,53 +2,63 @@
 
 const e = React.createElement;
 
-const LikeButton = () => {
+const renderInput = (onChange, onClick, input) => {
+    const submit = e('input', {
+        id: 'submit', value: 'Try it out!', type: 'submit', onClick,
+    }, null);
+    const textarea = e('textarea', { id: 'input', value: input, onChange }, null);
+
+    return e('div', { id: 'inputContainer' }, 'INPUT', e('br'), textarea, e('br'), submit);
+};
+
+const renderOutput = (isErrorFound, content) => {
+    const output = e('div', { id: 'output', style: { color: isErrorFound ? 'red' : 'white' } }, content);
+
+    return e('div', { id: 'outputContainer' }, 'OUTPUT', e('br'), output);
+};
+
+const InputOutput = () => {
     const [inputState, setInputState] = React.useState('const foo = 123;');
     const [outputState, setOutputState] = React.useState('123');
+    const [isErrorFoundState, setIsErrorFoundState] = React.useState(false);
 
-    const onChange = (e) => {
-        setInputState(e.target.value);
-    }
+    const onChange = (event) => {
+        setInputState(event.target.value);
+    };
 
     const onClick = async () => {
-        const reqBody = { 'input': inputState };
+        const reqBody = { input: inputState };
 
-        var res = await fetch('/interpret', {
+        const res = await fetch('/interpret', {
             method: 'POST',
             body: JSON.stringify(reqBody),
             headers: {
                 'Content-Type': 'application/json',
             },
-        })
+        });
 
         try {
-            var json = await res.json();
-        } catch (e) {
-            console.error(e);
+            const json = await res.json();
+
+            setOutputState(json);
+            setIsErrorFoundState(json.startsWith('ERROR'));
+        } catch (err) {
+            console.error(err);
+
+            setOutputState(err);
+            setIsErrorFoundState(true);
         }
+    };
 
-        setOutputState(json);
-    }
+    return e('div', { id: 'inoutContainer' }, renderInput(onChange, onClick, inputState), renderOutput(isErrorFoundState, outputState));
+};
 
-    const input = e('textarea', { id: 'input', value: inputState, onChange }, null);
-    const submit = e('input', { type: 'submit', onClick }, null);
-    const inputContainer = e(
-        'div',
-        { id: 'inputContainer' },
-        'INPUT', e('br'), input, e('br'), submit
-    );
+const MainContainer = () => {
+    const navbar = e('div', { id: 'navbar' },
+        e('h1', {}, 'Monkey interpreter'));
 
-    const output = e('div', { id: 'output' }, outputState);
-    const outputContainer = e(
-        'div',
-        { id: 'outputContainer' },
-        'OUTPUT', e('br'), output
-    );
-
-
-    return e('div', { id: 'outerContainer' }, inputContainer, outputContainer);
-}
-
+    return e('div', { id: 'outerContainer' }, navbar, e(InputOutput, {}, null));
+};
 
 const domContainer = document.querySelector('#app');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(MainContainer), domContainer);
