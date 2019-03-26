6beb184664890f9db58615e30f4ec5b3dbc75907
radlinskii <radlinskiignacy@gmail.com>
2019-03-26 10:22:30 +0100

    code refactor
    Co-authored-by: agnieszka-miszkurka <miszkur@student.agh.edu.pl>

--- static/main.js
+++ static/main.js
@@ -2,91 +2,63 @@
 
 const e = React.createElement;
 
-class InputOutput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputState: 'const foo = 123;',
-            outputState: '123',
-            outputColor: 'white'
-        };
-    }
+const renderInput = (onChange, onClick, input) => {
+    const submit = e('input', {
+        id: 'submit', value: 'Try it out!', type: 'submit', onClick,
+    }, null);
+    const textarea = e('textarea', { id: 'input', value: input, onChange }, null);
 
-    setOutputState(newOutput) {
-        let color = 'white';
-        if (newOutput.substring(0,5) === 'ERROR'){
-            color = 'red';
+    return e('div', { id: 'inputContainer' }, 'INPUT', e('br'), textarea, e('br'), submit);
+};
+
+const renderOutput = (isErrorFound, content) => {
+    const output = e('div', { id: 'output', style: { color: isErrorFound ? 'red' : 'white' } }, content);
+
+    return e('div', { id: 'outputContainer' }, 'OUTPUT', e('br'), output);
+};
+
+const InputOutput = () => {
+    const [inputState, setInputState] = React.useState('const foo = 123;');
+    const [outputState, setOutputState] = React.useState('123');
+    const [isErrorFoundState, setIsErrorFoundState] = React.useState(false);
+
+    const onChange = (event) => {
+        setInputState(event.target.value);
+    };
+
+    const onClick = async () => {
+        const reqBody = { input: inputState };
+
+        const res = await fetch('/interpret', {
+            method: 'POST',
+            body: JSON.stringify(reqBody),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        try {
+            const json = await res.json();
+
+            setOutputState(json);
+            setIsErrorFoundState(json.startsWith('ERROR'));
+        } catch (err) {
+            console.error(err);
+
+            setOutputState(err);
+            setIsErrorFoundState(true);
         }
-        this.setState({
-            outputState: newOutput,
-            outputColor: color
-        })
-    }
+    };
 
-    setInputState(newInput) {
-        this.setState({
-            inputState: newInput
-        })
-    }
-
-    render() {
-        
-        const onClick = async () => {
-            const reqBody = { 'input': this.state.inputState };
-    
-            var res = await fetch('/interpret', {
-                method: 'POST',
-                body: JSON.stringify(reqBody),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-    
-            try {
-                var json = await res.json();
-            } catch (e) {
-                console.error(e);
-            }
-    
-            this.setOutputState(json);
-        }
-        
-        const submit = e('input', { type: 'submit', onClick }, null);
-        
-        const onChange = (e) => {
-            this.setInputState(e.target.value);
-        }
-
-        const input = e('textarea', { id: 'input', value: this.state.inputState, onChange }, null);
-
-        const inputContainer = e(
-            'div',
-            { id: 'inputContainer' },
-            'INPUT', e('br'), input, e('br'), submit
-        );
-
-        const output = e('div', {id: 'output', style: {color: this.state.outputColor} }, this.state.outputState);
-
-        const outputContainer = e(
-            'div',
-            { id: 'outputContainer' },
-            'OUTPUT', e('br'), output
-        );
-
-        return e('div', {id: 'inoutContainer'}, inputContainer, outputContainer);
-    }
-}
+    return e('div', { id: 'inoutContainer' }, renderInput(onChange, onClick, inputState), renderOutput(isErrorFoundState, outputState));
+};
 
 const MainContainer = () => {
+    const navbar = e('div', { id: 'navbar' },
+        e('h1', {}, 'Monkey interpreter'));
 
-
-    const navbar = e('div', { id: 'navbar'}, 
-        e('h1', {}, 'Monkey interpreter')
-    );
-
-    return e('div', { id: 'outerContainer' }, navbar, 
-    e(InputOutput, {}, null));
-}
+    return e('div', { id: 'outerContainer' }, navbar, e(InputOutput, {}, null));
+};
 
 const domContainer = document.querySelector('#app');
 ReactDOM.render(e(MainContainer), domContainer);
