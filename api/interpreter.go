01880474df21a96e434bd5c8bcc2a146371d3660
radlinskii <radlinskiignacy@gmail.com>
2021-11-26 17:40:22 +0100

    fix api route

--- api/interpreter.go
+++ api/interpreter.go
@@ -22,93 +22,54 @@ type RequestObject struct {
 	Val string `json:"input"`
 }
 
-InterpreterHandler := http.TimeoutHandler(http.HandlerFunc(handleInterpret), 3*time.Second, "ERROR: Evaluation timeout")
-
-func handleInterpret(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.NotFoundHandler()
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	var reqObject RequestObject
-
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&reqObject)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	input := string(reqObject.Val)
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
-
-	var response string
-
-	if len(p.Errors()) != 0 {
-		for _, msg := range p.Errors() {
-			if strings.HasPrefix(msg, "FATAL") {
-				response += msg + "\n"
-				break
-			}
-			response += "ERROR: " + msg + "\n"
-		}
-	} else {
-		env := object.NewEnvironment()
-		response = evaluator.EvalProgram(program, env)
-	}
-
-	if res, err := json.Marshal(response); err != nil {
-		_, err = w.Write([]byte(err.Error()))
-	} else {
-		_, err = w.Write(res)
-	}
-
-	if err != nil {
-		log.Printf("Write failed: %v", err)
-	}
-
+func InterpreterHandler(w http.ResponseWriter, r *http.Request) {
+	return http.TimeoutHandler(http.HandlerFunc(createInterpreterHandlerFunction(w, r)), 3*time.Second, "ERROR: Evaluation timeout")
 }
 
-// UseCache adds cache-control header.
-func UseCache(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Cache-Control", "max-age=31536000")
-		h.ServeHTTP(w, r)
-	})
-}
-
-// UseGZip uses middleware for compressing response using gzip algorithm.
-func UseGzip(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		encodings := r.Header.Get("Accept-Encoding")
-		if !strings.Contains(encodings, "gzip") {
-			h.ServeHTTP(w, r)
+func createInterpreterHandlerFunction(w http.ResponseWriter, r *http.Request) {
+	return func () {
+		if r.Method != http.MethodPost {
+			http.NotFoundHandler()
 			return
 		}
-		w.Header().Add("Content-Encoding", "gzip")
-		gw := gzip.NewWriter(w)
-		defer gw.Close()
-		grw := gzipResponseWriter{
-			ResponseWriter: w,
-			Writer:         gw,
+		w.Header().Set("Content-Type", "application/json")
+
+		var reqObject RequestObject
+
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&reqObject)
+		if err != nil {
+			fmt.Println(err)
 		}
-		h.ServeHTTP(&grw, r)
-	})
-}
 
-type gzipResponseWriter struct {
-	http.ResponseWriter
-	io.Writer
-	status int
-}
+		input := string(reqObject.Val)
+		l := lexer.New(input)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-func (grw *gzipResponseWriter) WriteHeader(statusCode int) {
-	grw.status = statusCode
-	grw.ResponseWriter.WriteHeader(statusCode)
-}
+		var response string
 
-func (grw *gzipResponseWriter) Write(data []byte) (int, error) {
-	return grw.Writer.Write(data)
+		if len(p.Errors()) != 0 {
+			for _, msg := range p.Errors() {
+				if strings.HasPrefix(msg, "FATAL") {
+					response += msg + "\n"
+					break
+				}
+				response += "ERROR: " + msg + "\n"
+			}
+		} else {
+			env := object.NewEnvironment()
+			response = evaluator.EvalProgram(program, env)
+		}
+
+		if res, err := json.Marshal(response); err != nil {
+			_, err = w.Write([]byte(err.Error()))
+		} else {
+			_, err = w.Write(res)
+		}
+
+		if err != nil {
+			log.Printf("Write failed: %v", err)
+		}
+	}
 }
